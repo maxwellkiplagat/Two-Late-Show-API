2f292a5a274308f5c47175a41c8fe9deb3d68b6f
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-06-23 09:27:55 +0300

    added the app.py

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from server.config import SQLALCHEMY_DATABASE_URI, JWT_SECRET_KEY
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+
+from server.models import *
+from server.controllers.auth_controller import auth_bp
+from server.controllers.guest_controller import guest_bp
+from server.controllers.episode_controller import episode_bp
+from server.controllers.appearance_controller import appearance_bp
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(guest_bp)
+app.register_blueprint(episode_bp)
+app.register_blueprint(appearance_bp)
