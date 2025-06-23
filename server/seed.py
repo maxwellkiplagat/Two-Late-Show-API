29d2c2076043f5b66611116a301b3b5ca5320ca5
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-06-24 00:06:56 +0300

    updated my seeding data

--- server/seed.py
+++ server/seed.py
@@ -1,6 +1,7 @@
 from server.app import app, db
 from server.models.guest import Guest
 from server.models.episode import Episode
+from server.models.user import User
 from datetime import date
 
 with app.app_context():
@@ -13,6 +14,10 @@ with app.app_context():
     e1 = Episode(date=date(2023, 4, 20), number=101)
     e2 = Episode(date=date(2023, 4, 21), number=102)
 
-    db.session.add_all([g1, g2, e1, e2])
+    u1 = User(username="admin")
+    u1.set_password("password123")
+
+    db.session.add_all([g1, g2, e1, e2, u1])
     db.session.commit()
-    print("Smooth as butter ")
+
+    print("Smooth as butter 🧈")
