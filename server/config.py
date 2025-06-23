c2c5cc2b94084dc96c42efa97ad6b48fbfaca064
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-06-23 23:08:29 +0300

    changed identity to take a stringcd

--- server/config.py
+++ server/config.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:<password>@localhost:5432/late_show_db"
+SQLALCHEMY_DATABASE_URI = "postgresql://max:xyz@localhost:5432/late_show_db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = "your-secret-key"
