0faea503f8279d9c15886998a7724b8da9fb98f2
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-06-23 10:04:16 +0300

    added the dummy data in seed.py

---
+++ server/seed.py
@@ -0,0 +1,18 @@
+from server.app import app, db
+from server.models.guest import Guest
+from server.models.episode import Episode
+from datetime import date
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    
+    g1 = Guest(name="eliud kipchoge", occupation="marathon runner")
+    g2 = Guest(name="maxwell kiplagat", occupation="software developer")
+    
+    e1 = Episode(date=date(2023, 4, 20), number=101)
+    e2 = Episode(date=date(2023, 4, 21), number=102)
+
+    db.session.add_all([g1, g2, e1, e2])
+    db.session.commit()
+    print("Smooth as butter ")
